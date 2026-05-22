e41a7e131b149e645f2d22df72bb7ac1fa973e7d
Teodor Dimitrov <teodor.h.dimitrov.2024@elsys-bg.org>
2026-05-22 16:39:34 +0300

    Got product data

--- scraper.py
+++ scraper.py
@@ -8,9 +8,14 @@ def get_product_details(url):
     if response.status_code != 200:
         raise Exception(f"response didnt return status code 200, but status code {response.status_code}")
     
-    return response.content
+    data = response.json()
+
+    products = []
+
+    for item in data["Items"]:
+        name = item["Name"]
+        price = float(item["Specific"]["PriceMap"][0]["Price"][0]["Amount"])
+        products.append({"name":name, "price":price})
+    return products
 
 print(get_product_details("https://www.a1.bg/mgw-web/eshop/products/grid?PriceTypeFilter=2&DeviceType=Smartphone&PresetCollectionId=1&CurrentPage=1&perPage=100"))
-
-
-
